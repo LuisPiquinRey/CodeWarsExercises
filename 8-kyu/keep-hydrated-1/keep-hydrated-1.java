d4846934213115561ab9000946a008bcc5b4f035
Luis Piquin Rey <piquin.rey@gmail.com>
2025-05-29 07:59:54 +0200

    Add solution - CodeHub

---
+++ 8-kyu/keep-hydrated-1/keep-hydrated-1.java
@@ -0,0 +1,7 @@
+public class KeepHydrated  {
+  public static int liters(double time)  {    
+    //Your code goes here! Hint: You should change that -1
+    return (int)Math.floor(time*0.5);
+  }
+}
+​
