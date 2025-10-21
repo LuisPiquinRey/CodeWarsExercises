e640c21a723f4ce27b98367e12160b5c24068966
Luis Piquin Rey <piquin.rey@gmail.com>
2025-10-21 12:29:38 +0200

    Add solution - CodeHub

---
+++ 8-kyu/sum-of-positive/sum-of-positive.java
@@ -0,0 +1,10 @@
+import java.util.stream.IntStream;
+public class Positive{
+​
+  public static int sum(int[] arr){
+    return IntStream.of(arr)
+      .filter(e -> e > 0)
+      .sum();
+  }
+​
+}
